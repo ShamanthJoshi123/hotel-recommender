be3b683a89d915d93d679cf5adf812bffdb6978b
ShamanthJoshi <shamanthjoshi2004@gmail.com>
2025-08-10 23:24:43 +0530

    Deploy test

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ import glob
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins=["https://hotel-recommender.vercel.app/"])
+CORS(app)
 
 # Path to local OYO data CSV
 OYO_CSV_PATH = 'OYO_HOTELS_792_transformed.csv'
